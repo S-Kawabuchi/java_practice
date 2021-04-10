e48cb8f97e482f6bf0bf490ce2fd9ea917e27569
Shotaro <sk.jack.0712@gmail.com>
2021-04-10 10:44:17 +0900

    practice10

---
+++ practice10.java
@@ -0,0 +1,9 @@
+public class practice10 {
+    public static void main(String[] args){
+        int x = 5;
+
+        System.out.println("xの2乗="+(x*x));
+        System.out.println("xの3乗="+(x*x*x));
+        System.out.println("xの4乗="+(x*x*x*x));
+    }
+}

e6ce3f5472d868d1073e8bd53de9c1082f61134b
Shotaro <sk.jack.0712@gmail.com>
2021-04-10 10:57:53 +0900

    practice12

---
+++ practice12.java
@@ -0,0 +1,14 @@
+public class practice12 {
+    public static void main(String[] args){
+
+        int x = 5;
+        x++;
+        
+        System.out.println(x);
+        
+        x--;
+        x--;
+        System.out.println(x);
+
+    }
+}

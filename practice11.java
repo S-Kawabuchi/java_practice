604f941ed4fa2df6b7a7a100a04d21b32343d6dd
Shotaro <sk.jack.0712@gmail.com>
2021-04-10 10:57:35 +0900

    practice11

---
+++ practice11.java
@@ -0,0 +1,9 @@
+public class practice11 {
+    public static void main(String[] args){
+        
+        int x = 5;
+        int y = x-1;
+
+        System.out.println("商="+(x/y)+","+"余="+(x%y));
+    }
+}

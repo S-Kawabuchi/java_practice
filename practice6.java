cd99ba238fe416bb18e77b724361d3fd21e497f8
Shotaro <sk.jack.0712@gmail.com>
2021-04-10 09:32:36 +0900

    practice6

---
+++ practice6.java
@@ -0,0 +1,9 @@
+public class practice6 {
+    public static void main(String[] args){
+        int x = 7;
+        int y = 3;
+        double z = 0.5;
+        System.out.println("x*3="+(x*y));
+        System.out.println("x*z="+(x*z));
+    }
+}

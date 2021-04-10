2f0001946abbfd25aaffbd5f65f3b67655e72d89
Shotaro <sk.jack.0712@gmail.com>
2021-04-10 09:41:55 +0900

    practice8

---
+++ practice8.java
@@ -0,0 +1,12 @@
+public class practice8 {
+    public static void main(String[] args){
+        int x,y,z;
+
+        x = 3;
+        y = 4;
+
+        z = x*y;
+
+        System.out.println( "z="+z );
+    }
+}
